7bdf500c001b4f6de982bf7e678fed01d80258ea
dvranic_code <archiplace@gmail.com>
2018-10-09 15:04:02 +0200

    App done

--- Number_Guesser/app.js
+++ Number_Guesser/app.js
@@ -10,7 +10,7 @@ GAME FUNCTION
 // Game values
 let min = 1, 
     max = 10,
-    winningNum = 2,
+    winningNum = getWinningNum(min, max),
     guessesLeft =3;
 
 // UI Elements
@@ -35,23 +35,31 @@ guessBtn.addEventListener('click', function(){
   // Validate
   if(isNaN(guess) || guess<min || guess>max){
     setMessage(`Please choose number between ${min} and ${max}`, 'red');
+    guessInput.value = '';
+    return;
   }
 
   // check if won
   if(guess === winningNum){
-    // disable input
-    guessInput.disabled = true;
-    // change border to green
-    guessInput.style.borderColor = 'green';
-    // let user that they have won
-    setMessage(`${winningNum} is correct! YOU WIN`, 'green');
-    // change btn to play again
-    guessBtn.value = 'Play again';
-    guessBtn.addEventListener('click', function(){
-      location.reload();
-    })
+    gameOver(true, `${winningNum} is correct! YOU WIN`);
   } else {
 
+    // lose case
+    // Wrong number
+    guessesLeft -= 1;
+
+    // check to se if guess is left
+    if(guessesLeft === 0) {
+      // Game over
+      gameOver(false, `${winningNum} was correct answer. YOU LOST`);
+    } else {
+      // Game continue wrong answer
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left...`, 'red');
+      // change border to red
+      guessInput.style.borderColor = 'red';
+
+      guessInput.value = '';
+    }
   }
 
 });
@@ -60,4 +68,28 @@ guessBtn.addEventListener('click', function(){
 function setMessage(msg, color = 'gray'){
   message.style.color = color;
   message.textContent = msg;
+}
+
+// function GAME OVER
+function gameOver(won, msg) {
+
+  let color;
+  won === true ? color = 'green' : color = 'red';
+
+  // disable input
+  guessInput.disabled = true;
+  // change border to green
+  guessInput.style.borderColor = color;
+  // let user that they have won
+  setMessage(msg, color);
+  // change btn to play again
+  guessBtn.value = 'Play again';
+  guessBtn.addEventListener('click', function(){
+    location.reload();
+  });
+}
+
+// set winninng num
+function getWinningNum(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
